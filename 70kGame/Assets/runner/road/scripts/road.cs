d777f4cc0bf30afd47b09d89281babc03ece069a
Nikitaminon <Aoaoavtonomov@mail.ru>
2023-10-12 20:02:11 +0500

    бесконечный уровень

---
+++ 70kGame/Assets/runner/road/scripts/road.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class road : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(-transform.forward * _speed * Time.fixedDeltaTime);
+    }
+}
